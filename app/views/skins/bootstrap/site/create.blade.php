78e72ca8fdadd6c926d3192186fd236ae6c3b1f8
Ante de Baas <antedaxara@users.noreply.github.com>
2015-08-31 08:45:52 +0200

    set html maxlength to 50 aswell

--- app/views/skins/bootstrap/site/create.blade.php
+++ app/views/skins/bootstrap/site/create.blade.php
@@ -19,7 +19,7 @@
 					{{
 						Form::text('title', $paste->title, array(
 							'class'         => 'form-control',
-							'maxlength'     => 30,
+							'maxlength'     => 50,
 							'placeholder'   => Lang::get('global.paste_title'),
 							'disabled'      => $disabled
 						))
